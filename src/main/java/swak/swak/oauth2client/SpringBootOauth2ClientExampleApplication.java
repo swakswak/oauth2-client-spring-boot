24cf6d61578e19bff5b8a17372f0e0366bee069c
swakswak <hschoi0702@gmail.com>
2022-06-14 21:55:42 +0900

    init

---
+++ src/main/java/swak/swak/oauth2client/SpringBootOauth2ClientExampleApplication.java
@@ -0,0 +1,13 @@
+package swak.swak.oauth2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2ClientExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2ClientExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/swak/swak/oauth2client/SpringBootOauth2ClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package swak.swak.oauth2client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2ClientExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
