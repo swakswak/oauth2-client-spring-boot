e5801b62f649b928bde251e0c51329d9988f5ecb
swakswak <hschoi0702@gmail.com>
2022-06-19 02:30:00 +0900

    주석 제거 및 configure() 라인 변경

--- src/main/java/swak/swak/oauth2client/configurations/SecurityConfig.java
+++ src/main/java/swak/swak/oauth2client/configurations/SecurityConfig.java
@@ -17,30 +17,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .oauth2Login(oAuth2LoginConfig ->
-                        oAuth2LoginConfig
-                                .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
-                                        .userService(this.oAuth2UserService())
-                                )
-                                .redirectionEndpoint(redirectionEndpointConfig -> redirectionEndpointConfig
-                                        .baseUri("/api/auth/{action}/oauth2/code/{registrationId}")
-                                )
-                                .successHandler(oAuth2AuthenticationSuccessHandler)
-                )
-
+                .oauth2Login(oAuth2LoginConfig -> oAuth2LoginConfig
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(this.oAuth2UserService()))
+                        .redirectionEndpoint(redirectionEndpointConfig -> redirectionEndpointConfig
+                                .baseUri("/api/auth/{action}/oauth2/code/{registrationId}"))
+                        .successHandler(oAuth2AuthenticationSuccessHandler))
                 .sessionManagement(sessionManagementConfig -> sessionManagementConfig
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-        ;
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
     }
 
     @Bean
     OAuth2UserService oAuth2UserService() {
         return new OAuth2UserService();
     }
-
-//    @Bean
-//    OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler() {
-//        return new OAuth2AuthenticationSuccessHandler();
-//    }
 }
